23d40705711560ad580cb4efb9141cffaa61c9d4
ricegood <yeonjae.koo@gmail.com>
2018-10-21 21:20:41 +0900

    fix comment bug

--- skeleton/test.c
+++ skeleton/test.c
@@ -5,10 +5,14 @@ int main(){
 
 	a = 10;
 	b = 5;
-
+	/* /* hihihihihihhi */
 	if ( a == 10 || b == 5 ){
 		return 1;
 	} else {
 		return 0;
-	}
+	} */
+	hfisdhfladfkaflafdjadfsfa = 5;
+	
+	a = 5;
+
 }
