f7782c2817b3b7e79b2d95eb1794a53616d466ee
ricegood <951227@gmail.com>
2018-11-15 14:02:59 +0900

    create symboltable.c

---
+++ skeleton/symboltable.c
@@ -0,0 +1,8 @@
+/***************************************************************
+ * File Name    : symboltable.c
+ * Description
+ *      This is an implementation file for scope stack & symbol table
+ *
+ ****************************************************************/
+
+#include "subc.h"
