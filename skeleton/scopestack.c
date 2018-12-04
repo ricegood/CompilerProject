32fa913b32159ae5852e178048c62b648001143d
ricegood <951227@gmail.com>
2018-12-05 00:09:05 +0900

    print decl size

--- skeleton/scopestack.c
+++ skeleton/scopestack.c
@@ -24,7 +24,7 @@ void printscopestack(){
         if(ste_it->name == NULL || ste_it->decl == NULL)
           printf("ste_it->name == NULL || ste_it->decl == NULL\n");
         else
-          printf ("node name : %s, decl class : %d\n", ste_it->name->name, ste_it->decl->declclass);
+          printf ("node name : %s, decl class : %d, size : %d\n", ste_it->name->name, ste_it->decl->declclass, ste_it->decl->size);
         ste_it = ste_it->prev;
       }
       node_it = node_it->next;
@@ -35,7 +35,7 @@ void printscopestack(){
       if(ste_it->name == NULL || ste_it->decl == NULL)
           printf("ste_it->name == NULL || ste_it->decl == NULL\n");
         else
-          printf ("node name : %s, decl class : %d\n", ste_it->name->name, ste_it->decl->declclass);
+          printf ("node name : %s, decl class : %d, size : %d\n", ste_it->name->name, ste_it->decl->declclass, ste_it->decl->size);
       ste_it = ste_it->prev;
     }
   }
