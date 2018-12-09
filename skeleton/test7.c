b9492dd5cd86a41ab871dc39f6eb8dfa4f0e035a
ricegood <951227@gmail.com>
2018-12-09 13:08:41 +0900

    make test case for pointer structure return type

---
+++ skeleton/test7.c
@@ -0,0 +1,56 @@
+struct s {int x; int y[2]; int z;} s0;
+
+struct s *foo2(int a) {
+	s0.x = a+13;
+	s0.y[0] = 7;
+	s0.y[1] = a;
+	s0.z = a-2;
+	return &s0;
+}
+
+
+void main() {
+	
+	struct s s1;
+	struct s *s2;
+
+/*	s1 = *foo2(5); */
+	s2 = foo2(5);
+
+	write_int(s1.x); /* 18 */
+	write_string("\n");
+	write_int(s1.y[0]); /* 7 */
+	write_string("\n");
+	write_int(s1.y[1]); /* 5 */
+	write_string("\n");
+	write_int(s1.z); /* 3 */
+	write_string("\n=====================\n");
+
+	write_int(s2->x); /* 18 */
+	write_string("\n");
+	write_int(s2->y[0]); /* 7 */
+	write_string("\n");
+	write_int(s2->y[1]); /* 5 */
+	write_string("\n");
+	write_int(s2->z); /* 3 */
+	write_string("\n=====================\n");
+/*
+	write_int(*(foo2(5)).x); /* 18 */
+	write_string("\n");
+	write_int(*(foo2(5)).y[0]); /* 7 */
+	write_string("\n");
+	write_int(*(foo2(5)).y[1]); /* 5 */
+	write_string("\n");
+	write_int(*(foo2(5)).z); /* 3 */
+	write_string("\n=====================\n");
+*/
+	write_int(s0.x); /* 18 */
+	write_string("\n");
+	write_int(s0.y[0]); /* 7 */
+	write_string("\n");
+	write_int(s0.y[1]); /* 5 */
+	write_string("\n");
+	write_int(s0.z); /* 3 */
+	write_string("\n=====================\n");
+
+}
