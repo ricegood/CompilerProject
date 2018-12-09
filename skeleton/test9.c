0167d2b25029bd706e6cbccff5e34fbfe95b36a9
ricegood <951227@gmail.com>
2018-12-09 22:12:31 +0900

    if/negate/not TEST OK!

--- skeleton/test9.c
+++ skeleton/test9.c
@@ -3,10 +3,18 @@ int main(){
 	int b;
 	int x;
 
-	a = 4;
-	b = 5;
-	x = -3;
-/*
+	a = 0;
+	b = 1;
+	x = -5;
+
+	if (x < -a) {
+		write_string("x < -a\n");
+	} else if (x < -b) {
+		write_string("x < -b\n");
+	} else {
+		write_string("x >= -a && x>= -b\n");
+	}
+
 	if (a == b || x < 5) {
 		write_string("or check\n");
 	}
@@ -22,35 +30,31 @@ int main(){
 	} else {
 		write_string("a == b\n");
 	}
-*/
+
 	if (a < b) {
-		/*write_string("a < b\n");*/
+		write_string("a < b\n");
 		if (x > 0) {
 			write_string("x > 0\n");
-			/*
+			
 			if (x < 5) {
 				write_string("0 < x < 5\n");
 			}
 			else {
 				write_string("x >= 5\n");
 			}
-			*/
+			
 		} else if (x > -10) {
-			/*
 			write_string("-10 < x < 0\n");
 			if (x == -5) {
 				write_string("x == -5\n");
 			}
-			*/
 			write_string("x > -10\n");
 		}
 		else {
 			write_string("x <= -10\n");
 		}
 	}
-	/*
 	else {
 		write_string("a >= b\n");
 	}
-	*/
 }
