1c8efc9eb4c2fb58de0a16a1720282a9903dc69b
ricegood <951227@gmail.com>
2018-12-17 17:31:33 +0900

    error.c

--- skeleton/error.c
+++ skeleton/error.c
@@ -1,4 +1,4 @@
 void main() {
-	write_string("-----------------------"); // 23 or 24 characters : error
-	write_string("aaaaaaaaaaaaaaaaaaaaaaa"); // && 23 characters : error
+	write_string("-----------------------"); /* 23 or 24 characters : error */
+	write_string("aaaaaaaaaaaaaaaaaaaaaaa"); /* && 23 characters : error */
 }
