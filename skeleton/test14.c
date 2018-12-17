8fcb4efb19ccc78c1b63c3d909d90af8a8f62bf5
ricegood <951227@gmail.com>
2018-12-17 11:20:13 +0900

    for test

--- skeleton/test14.c
+++ skeleton/test14.c
@@ -1,4 +1,4 @@
-/* test from ppt page 6 */
+/* test from ppt page 6 + question */
 
 int a;
 int b[20];
@@ -13,6 +13,8 @@ int foo(int *d) {
 
 void main() {
 	int g;
+	int i;
+
 	a = 4;
 	b[1] = 6;
 	c.x = 3;
@@ -20,4 +22,19 @@ void main() {
 	g = 6;
 	write_int(foo(&g)); /* 146 */
 	write_string("\n");
+
+	for (i=270;i<272;) {
+		write_int(i++); /* 270 271 */
+		write_string("\n");
+	}
+	write_string("===============\n");
+	for (;i<=275;) {
+		write_int(i++); /* 272 273 274 275 */
+		write_string("\n");
+	}
+
+	for (;;) {
+		write_string("this is ?\n");
+		break;
+	}
 }
